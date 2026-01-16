102cfe9665a86336fa166ffe5ee2e184359dde68
katotokabesha-sys <katotokabesha@gmail.com>
2026-01-16 12:48:13 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'lbk-smart-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/base.css',
+  '/assets/js/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
